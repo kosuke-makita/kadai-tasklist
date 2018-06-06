ede3c23c1de39f175a88a892b20ada17dee55690
kosuke.makita <kosuke.makita@rakuten.com>
2018-06-06 06:01:36 +0000

    user registeration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to the Kadai-Tasklist！</h1>
+            {!! link_to_route('signup.get', '今すぐ登録!', null, ['class' => 'btn btn-lg btn-info']) !!}
         </div>
     </div>
 @endsection
